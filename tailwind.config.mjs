a04f19606b5418b18e90e367fae6aa7d7309c8fb
desingkuro <andresmauricio745@gmail.com>
2023-12-02 10:34:51 -0500

    pagina landing de tesla

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily:{
+			sans:['Gotham SSm A','sanas-serif']
+		},
+		extend: {},
+		screens:{
+			'sm':'720px'
+		}
+	},
+	plugins: [],
+}
